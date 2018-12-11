54a249d2496d376eb80fadacd9791573c33f0f10
alexey-kashapov <mailofkashapov@gmail.com>
2018-12-11 20:40:09 +0300

    some improvements

--- eurobot_stm/scripts/STM_protocol.py
+++ eurobot_stm/scripts/STM_protocol.py
@@ -6,7 +6,7 @@ import rospy
 
 class STMprotocol(object):
     def __init__(self,serial_port, baudrate=115200):
-        self.ser = serial(serial_port,baudrate=baudrate, timeout = 0.01)
+        self.ser = serial.Serial(serial_port,baudrate=baudrate, timeout = 0.01)
         self.pack_format = {
             0x01: "=cccc",
             0x07: "=",
@@ -19,28 +19,36 @@ class STMprotocol(object):
         self.unpack_format = {
             0x01: "=cccc",
             0x07: "=fff",
-            0x08: "=cc",
+            0x08: "=ccc",
             0x09: "=fff",
             0x0e: "=cc",
             0x0f: "=fff",
         }
+	
+	self.response_bytes = {
+	    0x01: 4,
+            0x07: 12,
+	    0x08: 2,
+            0x09: 12,
+            0x0e: 2,
+            0x0f: 12,
+	}
         
     def pure_send_command(self, cmd, args):
         # Clear buffer
         self.ser.reset_output_buffer()
         self.ser.reset_input_buffer()
         # Sending command
-        parameters = bytearray(struct.pack(self.pack_format[cmd], *args))
-        print ('cmd=', cmd)
-        msg = bytearray([cmd]) + parameters
-        print ('msg = ' + str(struct.unpack('=cfff',msg)))
+	print ('msg=', cmd , args)
+        msg = bytearray([cmd])
+        if args :
+            parameters = bytearray(struct.pack(self.pack_format[cmd], *args))
+            msg += parameters
         self.ser.write(msg)
-        
-        response = self.ser.readline()
-        print(response)
+        response = self.ser.read(self.response_bytes[cmd]+1)
+	print response
         if len(response) == 0:
             raise Exception("No data received")
-            
         values = struct.unpack(self.unpack_format[cmd], response)
         return True, values
         
@@ -51,8 +59,9 @@ class STMprotocol(object):
                 return self.pure_send_command(cmd, args)
             except Exception as exc:
                 if i == n_repeats - 1:
-                    rospy.logerr('Exception:\t', exc)
-                    rospy.logerr('At time:\t', datetime.datetime.now())
-                    rospy.logerr('cmd:', cmd, 'args:', args)
-                    rospy.logerr('--------------------------')
+                    rospy.loginfo('Exception:\t' + str(exc))
+                    rospy.loginfo('At time:\t' + str(datetime.datetime.now()))
+                    rospy.loginfo('cmd:' + str(cmd) + 'args:' + str(args))
+                    #rospy.loginfo()
+                    rospy.loginfo('--------------------------')
         return False, None
