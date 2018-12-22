7200636e3842a063eea13193dcfab8d3010da578
nickzherdev <nikolay.zherdev@gmail.com>
2018-12-22 19:11:31 +0300

    improvements

--- eurobot_nav/scripts/arc_move_simple.py
+++ eurobot_nav/scripts/arc_move_simple.py
@@ -12,7 +12,7 @@ from std_msgs.msg import String
 from threading import Lock
 from std_msgs.msg import Int32MultiArray
 
-class MotionPlanner:
+class MotionPlannerNode:
     def __init__(self):
         rospy.init_node("motion_planner", anonymous=True)
 
@@ -32,10 +32,12 @@ class MotionPlanner:
 
         self.RATE = 10
 
-        self.XY_GOAL_TOLERANCE = 0.02
+        self.XY_GOAL_TOLERANCE = 0.1
         self.YAW_GOAL_TOLERANCE = 0.1
 
         self.vel = np.zeros(3)
+
+        self.path_left = 9999 # big initial value
 
         self.cmd_stop_robot_id = None
         self.stop_id = 0
@@ -46,11 +48,8 @@ class MotionPlanner:
 
         self.Kp = 5
 
-        self.THRESHOLD_XY = 0.1
-        self.THRESHOLD_YAW = 0.1
 
-
-        self.pub_twist = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+        #self.pub_twist = rospy.Publisher("cmd_vel", Twist, queue_size=1)
         self.pub_cmd = rospy.Publisher("/secondary_robot/stm_command", String, queue_size=1)
 
         rospy.Subscriber("move_command", String, self.cmd_callback, queue_size=1)
@@ -77,7 +76,8 @@ class MotionPlanner:
         rospy.loginfo("NEW CMD:\t" + str(data.data))
 
         # when new cmd arrives shutdown running timer
-        rospy.Timer.shutdown()
+        if self.timer is not None:
+            self.timer.shutdown()
 
         # parse name,type
         data_splitted = data.data.split()
@@ -90,10 +90,10 @@ class MotionPlanner:
             goal = args[:3]
             goal[2] %= (2 * np.pi)
             self.set_goal(goal, cmd_id, cmd_type)
-            rospy.Timer(rospy.Duration(1.0 / self.RATE), self.mode)
+            self.timer = rospy.Timer(rospy.Duration(1.0 / self.RATE), self.mode)
 
 
-        elif cmd_type == "stop":
+        elif cmd_type == "stop" or self.path_left < self.XY_GOAL_TOLERANCE and self.path_left < self.YAW_GOAL_TOLERANCE:
             self.cmd_id = cmd_id
             self.mode = cmd_type
             self.terminate_following()
@@ -155,51 +155,47 @@ class MotionPlanner:
         :return:
         """
 
-        try:
-            rospy.loginfo("-------NEW ARC MOVEMENT-------")
-            rospy.loginfo("Goal:\t" + str(goal))
+        rospy.loginfo("-------NEW ARC MOVEMENT-------")
+        rospy.loginfo("Goal:\t" + str(self.goal))
 
-            while not self.update_coords():
-                rospy.sleep(0.05)
+        while not self.update_coords():
+            rospy.sleep(0.05)
 
-            distance_map_frame = self.distance(self.coords, self.goal)
+        distance_map_frame = self.distance(self.coords, self.goal)
 
-            x_diff = distance_map_frame[0]
-            y_diff = distance_map_frame[1]
-            alpha = distance_map_frame[2] # theta_diff
+        x_diff = distance_map_frame[0]
+        y_diff = distance_map_frame[1]
+        alpha = distance_map_frame[2] # theta_diff
 
-            gamma = np.arctan2(y_diff, x_diff)
+        gamma = np.arctan2(y_diff, x_diff)
 
-            self.norm_d = np.linalg.norm(distance_map_frame[:2])
-            self.sqrt_d = np.sqrt(x_diff**2 + y_diff**2) # rho,
+        d_norm = np.linalg.norm(distance_map_frame[:2])
+        #sqrtD = np.sqrt(x_diff**2 + y_diff**2) # rho,
+        rospy.loginfo("euclidian distance left %.3f", d_norm)
 
-            path_done = np.sqrt(self.d_init**2 + self.alpha_init**2) - np.sqrt(d**2 + alpha**2)
-            path_left = np.sqrt(d**2 + alpha**2)
+        #path_done = np.sqrt(self.d_init**2 + self.alpha_init**2) - np.sqrt(d**2 + alpha**2)
+        self.path_left = np.sqrt(d_norm**2 + alpha**2) # in units of angle and distance
 
-            v = max(self.V_MIN, min(self.Kp * self.sqrt_d, self.V_MAX))
+        v = max(self.V_MIN, min(self.Kp * d_norm, self.V_MAX))
 
-            if alpha < 1e-4:
-                w = 0
-            else:
-                R = 0.5 * d / np.sin(alpha/2)
-                w = v / R  # must be depended on v such way so path becomes an arc
+        if alpha < 1e-4:
+            w = 0
+        else:
+            R = 0.5 * d_norm / np.sin(alpha/2)
+            w = v / R  # must be depended on v such way so path becomes an arc
 
-            beta = self.wrap_angle(gamma - alpha/2)
+        beta = self.wrap_angle(gamma - alpha/2)
 
-            vx = v * np.cos(beta)
-            vy = v * np.sin(beta)
-            vx, vy = self.rotation_transform(np.array([vx, vy]), -self.coords[2])
+        vx = v * np.cos(beta)
+        vy = v * np.sin(beta)
+        vx, vy = self.rotation_transform(np.array([vx, vy]), -self.coords[2])
 
-            v_cmd = np.array([vx, vy, w])
-            self.set_speed(v_cmd)
-
-
-        except Exception as msg:
-            rospy.logwarn(str(msg))
+        v_cmd = np.array([vx, vy, w])
+        self.set_speed(v_cmd)
 
 
     def line_move(self):
-
+        pass
 
     def update_coords(self):
         try:
@@ -215,5 +211,5 @@ class MotionPlanner:
 
 
 if __name__ == "__main__":
-    planner = MotionPlanner()
+    planner = MotionPlannerNode()
     rospy.spin()
