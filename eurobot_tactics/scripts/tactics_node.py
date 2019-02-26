21b6057d06f983de527154868e3c88c827400681
nickzherdev <nikolay.zherdev@gmail.com>
2019-02-26 20:23:59 +0300

    lil changes from robot

--- eurobot_tactics/scripts/tactics_node.py
+++ eurobot_tactics/scripts/tactics_node.py
@@ -7,7 +7,7 @@ from tf.transformations import euler_from_quaternion
 from visualization_msgs.msg import MarkerArray
 from std_msgs.msg import String
 from threading import Lock
-from manipulator import Manipulator
+# from manipulator import Manipulator
 
 # from geometry_msgs.msg import Twist
 # from std_msgs.msg import Int32MultiArray
@@ -66,8 +66,7 @@ class TacticsNode:
         self.approach_dist = 0.11  # meters, distance from robot to puck where robot will try to grab it
         self.approach_vec = np.array([-0.12, 0, 0])
         # self.approach_vec = np.array([-0.153, 0, 0])  # big robot
-        self.drive_back_dist = np.array([-0.05, 0, 0])
-        # self.drive_back_dist = np.array([-0.07, 0, 0])
+        self.drive_back_dist = np.array([-0.07, 0, 0])
 
         # self.coords_threshold = 0.01  # meters, this is variance of detecting pucks coords using camera, used in update
         self.scale_factor = 10  # used in calculating outer bissectrisa for hull's angles
@@ -101,11 +100,14 @@ class TacticsNode:
         rospy.sleep(2)
 
         self.timer = None
-
+        print("here is ok should be")
         # self.manipulator = Manipulator()
-        # if not self.manipulator.calibrate_big():
+        print("here is ok? init = Manip")
+        # if not self.manipulator.calibrate_small():
+        #     print("here shouldn't be")
         #     return
 
+        print("if here than all is ok")
         # coords are published as markers in one list according to 91-92 undistort.py
         # FIXME
         #  add /secondary_robot when in simulator, remove when on robot!!!!!!!!!!!
@@ -289,7 +291,7 @@ class TacticsNode:
 
         if self.operating_state == 'nearest LANDING approached' and not self.is_robot_collecting_puck:
             self.goal_landing = None
-            # self.is_puck_collected = self.manipulator.collect_big()
+            # self.is_puck_collected = self.manipulator.collect_small()
             self.is_robot_collecting_puck = True
             self.operating_state = 'collecting puck'
             rospy.loginfo(self.operating_state)
