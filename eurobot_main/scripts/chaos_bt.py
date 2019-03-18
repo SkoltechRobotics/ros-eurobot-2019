496475c8c3f9424485527d6991cec6d65821a95a
nickzherdev <nikolay.zherdev@gmail.com>
2019-03-18 20:27:46 +0300

    correct chaos bt and few changes from Misha to behavior_tree

--- eurobot_main/scripts/chaos_bt.py
+++ eurobot_main/scripts/chaos_bt.py
@@ -10,62 +10,45 @@ red_cell_puck = rospy.get_param("red_cell_puck")
 green_cell_puck = rospy.get_param("green_cell_puck")
 blue_cell_puck = rospy.get_param("blue_cell_puck")
 
-move_publisher = rospy.Publisher("navigation/command", String, queue_size=100)
-manipulator_publisher = rospy.Publisher("manipulator/command", String, queue_size=100)
-
-move_client = bt_ros.ActionClient(move_publisher)
-manipulator_client = bt_ros.ActionClient(manipulator_publisher)
-
 
 class CollectChaosPucks(bt.FallbackNode):
-
-    # get observation from camera
-    # is_chaos_collected_completely
-        # calculate_pucks_configuration
-        # calculate_landings
-        # move to first landing
-        # suck puck
-        # move back for safety
-        # finish suck + seq(calculate_pucks_configuration + calculate_landings) + move_to_next_prelanding
-
-
     def __init__(self, move_client, manipulator_client):
+
+        # Init parameters
+        self.pucks = bt.BTVariable([])
+        self.is_observed = bt.BTVariable(False)
+
+        # Init useful child nodes
+        self.move_to_waypoint_node = bt_ros.ActionClientNode("move 0 0 0", move_client, name="move_to_waypoint")
+        self.move_to_waypoint_node = bt_ros.ActionClientNode("move 0 0 0", manipulator_client, name="move_to_waypoint")
+        self.choose_new_waypoint_latch = bt.Latch(bt.ActionNode(self.choose_new_waypoint))
+
+        # Make BT
         super(CollectChaosPucks, self).__init__([
             bt.ConditionNode(self.is_chaos_collected_completely),
             bt.SequenceNode([
                 bt.SequenceWithMemoryNode([
                     bt.FallbackNode([
-                        bt.ConditionNode(self.is_chaos_observed),
-                        bt.ActionNode(self.get_chaos_observation_from_camera())  # FIXME
+                        ,
+                        bt.ActionNode(self.get_chaos_observation_from_camera)  # FIXME
                     ]),
+                    bt.ConditionNode(self.is_chaos_observed), # return RUNNING when not observed
 
-                    bt.FallbackNode([
-                        bt.ConditionNode(self.is_first_puck),
+                    bt.FallbackNodeWithMemory([
+                        bt.ConditionNode(self.is_not_first_puck),
                         bt.SequenceNodeWithMemory([
-                            bt.ActionNode(self.calculate_pucks_configuration()),
-                            bt.ActionNode(self.calculate_landings()),
-                            bt_ros.MoveArcToPoint(prelanding, "move_client"),  # approach_nearest_prelanding
+                            bt.ActionNode(self.calculate_pucks_configuration),
+                            bt.ActionNode(self.calculate_landings),
+                            bt_ros.MoveLineToPoint(prelanding, "move_client"),  # approach_nearest_prelanding
                             bt_ros.MoveLineToPoint(landings[0], "move_client"),
+                        ]),
                     ]),
 
-                    bt.FallbackNode([
-                        bt.ConditionNode(self.is_puck_sucked),
-                        bt.ConditionNode(self.is_first_puck)
-                        bt.SequenceNodeWithMemory([
-                            bt.FallbackNode([
-                                bt.ConditionNode(self.is_landing_approached),
-                                bt.SequenceNode([
-                                    # bt.ConditionNode(self.is_trajectory_collision_free),
-  # approach_nearest_landing
-                                ])
-                            ]),
-                            bt_ros.StartCollectGround("manipulator_client")  # self.start_suck()
-                        ])
-                    ]),
-                    bt.FallbackNode([
+                    bt_ros.StartCollectGround("manipulator_client"),  # self.start_suck()
+
+                    bt.FallbackNodeWithMemory([
                         bt.ConditionNode(self.is_safe_away_from_other_pucks_to_suck),
                         bt.SequenceNode([
-                            # bt.ConditionNode(self.is_trajectory_collision_free),
                             bt_ros.MoveLineToPoint(drive_back_point, "move_client")  # self.drive_back()
                         ])
                     ]),
@@ -76,78 +59,49 @@ class CollectChaosPucks(bt.FallbackNode):
                         bt.FallbackNodeWithMemory([
                             bt.ConditionNode(self.is_last_puck),
 
-                            bt.SequenceNode([
-
-                                # bt.ConditionNode(self.is_trajectory_collision_free),
-
-                                bt.SequenceNode([
-                                    bt.ActionNode(self.calculate_pucks_configuration()),
-                                    bt.ActionNode(self.calculate_landings())
-                                ]),
+                            bt.SequenceNodeWithMemory([
+                                bt.ActionNode(self.calculate_pucks_configuration),
+                                bt.ActionNode(self.calculate_landings)
                                 bt_ros.MoveArcToPoint(prelanding, "move_client"),  # approach_nearest_prelanding
                                 bt_ros.MoveLineToPoint(landings[0], "move_client"),  # approach_nearest_landing
                             ])
                         ])
-                    ], threshold=3),  # FIXME
+                    ], threshold=2),  # FIXME
                 ]),
                 bt.ConditionNode(lambda: bt.Status.RUNNING),
             ])
         ])
 
-#
+    def pucks_callback(self, data):
+        self.pucks.set(data)
+        self.is_observed.set(True)
 
-# CollectChaos = CollectChaosPucks(move_client, manipulator_client)
+    def is_chaos_observed(self):
+        if self.is_observed.get():
+            return bt.Status.SUCCESS
+        else:
+            return bt.Status.RUNNING
 
-bt_ros.MoveArcToPoint([0.99, 1.34, 1.57], "move_client")  # FIXME
-bt_ros.MoveLineToPoint([0.23, 0.44, 0], "move_client")  # FIXME
 
-class MainRobotBT:
+
+class MainRobotBT(object):
+    # noinspection PyTypeChecker
     def __init__(self):
-        rospy.init_node("chaos_bt")
+        rospy.init_node("CHAOS_BT")
 
-        self.robot_name = rospy.get_param("robot_name")
+        self.move_publisher = rospy.Publisher("navigation/command", String, queue_size=100)
+        self.manipulator_publisher = rospy.Publisher("manipulator/command", String, queue_size=100)
 
+        self.move_client = bt_ros.ActionClient(self.move_publisher)
+        self.manipulator_client = bt_ros.ActionClient(self.manipulator_publisher)
 
         rospy.Subscriber("navigation/response", String, self.move_client.response_callback)
-
-
         rospy.Subscriber("manipulator/response", String, self.manipulator_client.response_callback)
 
         rospy.sleep(2)
 
-        # Calibrate manipulator
-        default_state = bt.Latch(bt_ros.SetToDefaultState("manipulator_client"))
-
-
-        # fivth
-        move_5_puck = bt.Latch(bt_ros.MoveLineToPoint([0.99, 1.34, 1.57], "move_client"))
-        start_take_5_puck = bt.Latch(bt_ros.StartTakeWallPuck("manipulator_client"))
-        complete_take_5_puck = bt.Latch(bt_ros.CompleteTakeWallPuck("manipulator_client"))
-        move_5_back = bt.Latch(bt_ros.MoveLineToPoint([0.23, 0.44, 0], "move_client"))
-        set_man_up = bt.Latch(bt_ros.SetManipulatortoUp("manipulator_client"))
-        # parallel5 = bt.ParallelNode([move_5_back, set_man_up], threshold=2)
-        # Scales
-        move_scales1 = bt.Latch(bt_ros.MoveLineToPoint([1.34, 1.34, 1.57], "move_client"))
-        move_scales2 = bt.Latch(bt_ros.MoveLineToPoint([1.34, 1.44, 1.57], "move_client"))
-        move_back = bt.Latch(bt_ros.MoveLineToPoint([0.23, 0.44, 0], "move_client"))  
-
-        first_puck = bt.SequenceNode([parallel0, start_take_1_puck, parallel1])
-
-        second_puck = bt.SequenceNode([move_2_puck, start_take_2_puck, parallel2])
-
-        third_puck = bt.SequenceNode([move_3_puck, start_take_3_puck, parallel3])
-
-        forth_puck = bt.SequenceNode([move_4_puck, start_take_4_puck, parallel4])
-
-        fivth_puck = bt.SequenceNode([move_5_puck, start_take_5_puck, complete_take_5_puck, set_man_up])
-
-        scales = bt.SequenceNode([move_scales1, move_scales2, move_back])
-
-        pucks = bt.SequenceNode([first_puck, second_puck, third_puck, forth_puck, fivth_puck, scales])
-
-        leaf = bt.SequenceNode([default_state, pucks])
-
-        self.bt = bt.Root(leaf, action_clients={"move_client": self.move_client, "manipulator_client": self.manipulator_client})
+        self.bt = bt.Root(CollectChaosPucks("move_client", "manipulator_client"),
+                          action_clients={"move_client": self.move_client, "manipulator_client": self.manipulator_client})
 
         self.bt_timer = rospy.Timer(rospy.Duration(0.1), self.timer_callback)
 
@@ -157,7 +111,6 @@ class MainRobotBT:
             self.bt_timer.shutdown()
         print("============== BT LOG ================")
         self.bt.log(0)
-
 
 
 if __name__ == '__main__':
