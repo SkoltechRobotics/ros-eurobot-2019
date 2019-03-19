638519ec248b3d9f77ae8897800e781efac5963f
alexey-kashapov <mailofkashapov@gmail.com>
2019-03-19 16:38:39 +0300

    Egor commited smth

--- eurobot_loc/scripts/particle_filter/particle_filter_node.py
+++ eurobot_loc/scripts/particle_filter/particle_filter_node.py
@@ -34,9 +34,9 @@ class PFNode(object):
     # noinspection PyTypeChecker
     def __init__(self):
         # Init params
-        rospy.Subscriber("/secondary_robot/scan", LaserScan, self.scan_callback, queue_size=1)
         self.beacons = []
         self.robot_name = rospy.get_param("robot_name")
+        rospy.Subscriber("/%s/scan"%self.robot_name, LaserScan, self.scan_callback, queue_size=1)
         self.color = rospy.get_param("start_side")
         if self.color == "purple":
             beacons = PURPLE_BEACONS
@@ -70,15 +70,15 @@ class PFNode(object):
         self.laser_time = rospy.Time.now()
         self.cost_function = []
         if self.color == "purple":
-            init_start = rospy.get_param("start_purple")
+            init_start = np.array(rospy.get_param("start_purple"))
         else:
-            init_start = rospy.get_param("start_yellow")
+            init_start = np.array(rospy.get_param("start_yellow"))
         buf_pf = ParticleFilter(color=self.color, start_x=init_start[0], start_y=init_start[1], start_angle=init_start[2])
         angles, distances = buf_pf.get_landmarks(self.scan)
         x = distances * np.cos(angles)
         y = distances * np.sin(angles)
         landmarks = (np.array([x, y])).T
-        start_coords = find_position_triangulation(beacons, landmarks, np.array([1.5, 1, 0]))
+        start_coords = find_position_triangulation(beacons, landmarks, init_start)
         self.pf = ParticleFilter(color=self.color, start_x=start_coords[0], start_y=start_coords[1], start_angle=start_coords[2])
         self.last_odom = np.zeros(3)
         self.alpha = rospy.get_param("alpha")
@@ -191,10 +191,10 @@ class PFNode(object):
 
     def callback_frame(self, data):
         try:
-            if data.transforms[0].header.frame_id == "secondary_robot_odom":
+            if data.transforms[0].header.frame_id == "%s_odom"%self.robot_name:
                 self.prev_lidar_odom_time = self.lidar_odom_time
                 self.lidar_odom_time = data.transforms[0].header.stamp
-                odom = self.tf_buffer.lookup_transform("secondary_robot_odom", "secondary_robot", self.lidar_odom_time)
+                odom = self.tf_buffer.lookup_transform("%s_odom"%self.robot_name, self.robot_name, self.lidar_odom_time)
                 robot_odom_point = self.tf_to_coords(odom)
                 self.prev_lidar_odom_point_odom = self.lidar_odom_point_odom.copy()
                 self.lidar_odom_point_odom = robot_odom_point
