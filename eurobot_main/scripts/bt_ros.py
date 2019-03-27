a8456660c552de1edead6320f3eb77990048faf4
NUKA-COLA <mailofkashapov@gmail.com>
2019-03-27 14:43:23 +0300

    make beautiful style for bt

--- eurobot_main/scripts/bt_ros.py
+++ eurobot_main/scripts/bt_ros.py
@@ -70,48 +70,6 @@ class ActionClientNode(bt.SequenceNode):
         bt.BTNode.log(self, level, prefix)
 
 
-class STMClientNode(bt.SequenceNode):
-    def __init__(self, cmd, action_client_id, **kwargs):
-        self.action_client_id = action_client_id
-        self.cmd = bt.BTVariable(cmd)
-        self.cmd_id = bt.BTVariable()
-
-        self.start_node = bt.ActionNode(self.send_command)
-        bt.SequenceNode.__init__(self, [self.start_node, bt.ConditionNode(self.action_status)], **kwargs)
-
-    def send_command(self): 
-        self.cmd_id.set(self.root.action_clients[self.action_client_id].set_cmd(self.cmd.get()))
-
-    def action_status(self):
-        pass
-
-    def reset(self):
-        self.start_node.reset()
-
-    def log(self, level, prefix=""):
-        bt.BTNode.log(self, level, prefix)
-
-
-class isStartStatus(STMClientNode):
-    def __init__(self, action_client_id):
-        self.counter = 0
-        cmd = "3"
-        super(isStartStatus, self).__init__(cmd, action_client_id)
-
-    def action_status(self):
-        status = self.root.action_clients[self.action_client_id].get_status(self.cmd_id.get())
-        print ("STATUS=", status)
-        if status == "0":
-            self.counter = 0
-        elif status == "1":
-            self.counter += 1
-
-        if self.counter == 5:
-            return bt.Status.SUCCESS
-        else :
-            return bt.Status.RUNNING
-
-
 class SetToDefaultState(ActionClientNode):
     def __init__(self, action_client_id):
         cmd = "default"
