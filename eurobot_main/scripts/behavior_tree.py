c45e1c509a6aaec41c23ea5daba9583c10c03d9c
alexey-kashapov <mailofkashapov@gmail.com>
2019-03-28 16:45:24 +0300

    now can use function for nodes with arguments

--- eurobot_main/scripts/behavior_tree.py
+++ eurobot_main/scripts/behavior_tree.py
@@ -201,22 +201,37 @@ class ActionNode(BTNode):
     def __init__(self, function, **kwargs):
         assert callable(function)
         self.function = function
+        if "func_args" in kwargs.keys():
+            self.function_args = kwargs["func_args"]
+        else:
+            self.function_args = None
         super(ActionNode, self).__init__(**kwargs)
 
     def tick(self):
         ret = self.function()
+        if self.function_args is None:
+            ret = self.function()
+        else:
+            ret = self.function(self.function_args)
         assert ret is None
         return Status.SUCCESS
 
 
 class ConditionNode(BTNode):
-    def __init__(self, function, **kwargs):
+    def init(self, function, **kwargs):
         assert callable(function)
         self.function = function
-        super(ConditionNode, self).__init__(**kwargs)
+        if "func_args" in kwargs.keys():
+            self.function_args = kwargs["func_args"]
+        else:
+            self.function_args = None
+        super(ConditionNode, self).init(**kwargs)
 
     def tick(self):
-        self.status = self.function()
+        if self.function_args is None:
+            self.status = self.function()
+        else:
+            self.status = self.function(self.function_args)
         assert isinstance(self.status, Status)
         return self.status
 
